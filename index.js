5620763c0a4481fac354341c6c46b56fca3dace8
sanchezpepe <sanchezpepe97@gmail.com>
2020-06-07 14:28:17 -0500

    scrollbar and case component

--- index.js
+++ index.js
@@ -1,40 +1,73 @@
 var Cases = {
-   template: `
-      <div class="case box">
-         <strong>Data</strong>
-         <textarea class="textarea is-small" rows="7"></textarea>
-         <strong class="">Preconditions</strong>
-         <textarea class="textarea is-small" rows="7"></textarea>
-         <strong class="">Steps</strong>
-         <textarea class="textarea is-small" rows="7"></textarea>
-         <strong class="">Results</strong>
-         <textarea class="textarea is-small" rows="7"></textarea>
-      </div> `
+   template: 
+   `
+      <div class="case has-text-left">
+      <div class="field box">
+         <label class="label">Data</label>
+         <div class="control">
+            <textarea class="textarea" rows="1"></textarea>
+         </div>
+      </div>
+      <div class="field box">
+         <label class="label">Preconditons</label>
+         <div class="control">
+            <textarea class="textarea" rows="2"></textarea>
+         </div>
+      </div>
+      <div class="field box">
+         <label class="label">Steps</label>
+         <div class="control">
+            <textarea class="textarea" rows="7"></textarea>
+         </div>
+      </div>
+      <div class="field box">
+         <label class="label">Results</label>
+         <div class="control">
+            <textarea class="textarea" rows="3"></textarea>
+         </div>
+      </div>
+      <div>
+   `
 }
 
 var navbar = {
    template: `
-      <div style="height: 5%; display: flex; justify-content:space-between;">
-         <div class="subtitle">
-            Test Case Builder
-         </div>
-
-         <div class="buttons">
-            <a class="button is-link">
-               <strong>Import</strong>
-            </a>
-            <a class="button is-link">
-               <strong>Save cases</strong>
+   <div style="height: 5%; display: flex; justify-content:space-between;">
+      <div class="subtitle">
+         Test Case Builder
+      </div>
+      <div class="columns is-vcentered">
+         <div>
+            <a class="button is-small is-link">
+               <strong>Save</strong>
             </a>
          </div>
-      </div> `
+         <div id="file-js-example" class=" column file is-small has-name">
+            <label class="file-label">
+               <input class="file-input" type="file" name="resume">
+               <span class="file-cta">
+                  <span class="file-icon">
+                     <i class="fas fa-upload"></i>
+                  </span>
+                  <span class="file-label">
+                     Cargar archivo
+                  </span>
+               </span>
+               <span class="file-name">
+                  N/A
+               </span>
+            </label>
+         </div>
+      </div>
+   </div>
+   `
 }
 
 new Vue({
    el: '#app',
    components: {
       'Case': Cases,
-      'Navbar' : navbar
+      'Navbar': navbar
    }
 })
 
@@ -61,4 +94,13 @@ function showDivs(n) {
       x[i].style.display = "none";
    }
    x[slideIndex - 1].style.display = "block";
+}
+
+
+const fileInput = document.querySelector('#file-js-example input[type=file]');
+fileInput.onchange = () => {
+   if (fileInput.files.length > 0) {
+      const fileName = document.querySelector('#file-js-example .file-name');
+      fileName.textContent = fileInput.files[0].name;
+   }
 }
