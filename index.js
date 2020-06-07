7eacdcd068bfccb8a02bd083603be6286e9357b0
sanchezpepe <sanchezpepe97@gmail.com>
2020-06-07 15:08:07 -0500

    redisigned done cases block

--- index.js
+++ index.js
@@ -72,31 +72,6 @@ new Vue({
 })
 
 
-
-
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-   showDivs(slideIndex += n);
-}
-
-function showDivs(n) {
-   var i;
-   var x = document.getElementsByClassName("mySlides");
-   if (n > x.length) {
-      slideIndex = 1
-   }
-   if (n < 1) {
-      slideIndex = x.length
-   }
-   for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-   }
-   x[slideIndex - 1].style.display = "block";
-}
-
-
 const fileInput = document.querySelector('#file-js-example input[type=file]');
 fileInput.onchange = () => {
    if (fileInput.files.length > 0) {
