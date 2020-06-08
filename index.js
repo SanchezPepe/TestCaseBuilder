1843eb320883ce5a3ab75a2463b3e9dd0a2a6543
sanchezpepe <sanchezpepe97@gmail.com>
2020-06-07 20:49:16 -0500

    drag and drop

--- index.js
+++ index.js
@@ -1,29 +1,41 @@
 var Cases = {
-   template: 
-   `
+   data: function () {
+      return {
+         title: '',
+         data: '',
+         preconditons: '',
+         steps: '',
+         results: ''
+      }
+   },
+   template: `,
       <div class="case has-text-left">
+         <div class="field box">
+         <input class="input" placeholder="Test case title" v-model="title" ondragstart="dragstart_handler(event);" draggable="true"></input>
+      </div>
+
       <div class="field box">
          <label class="label">Data</label>
          <div class="control">
-            <textarea class="textarea" rows="1"></textarea>
+            <textarea class="textarea" rows="1" v-model='data' ondragstart="dragstart_handler(event);" draggable="true"></textarea>
          </div>
       </div>
       <div class="field box">
          <label class="label">Preconditons</label>
          <div class="control">
-            <textarea class="textarea" rows="2"></textarea>
+            <textarea class="textarea" rows="2" v-model="preconditons" ondragstart="dragstart_handler(event);" draggable="true"></textarea>
          </div>
       </div>
       <div class="field box">
          <label class="label">Steps</label>
          <div class="control">
-            <textarea class="textarea" rows="7"></textarea>
+            <textarea class="textarea" rows="4" v-model="steps" ondragstart="dragstart_handler(event);" draggable="true"></textarea>
          </div>
       </div>
       <div class="field box">
          <label class="label">Results</label>
          <div class="control">
-            <textarea class="textarea" rows="3"></textarea>
+            <textarea class="textarea" rows="2" v-model="results" ondragstart="dragstart_handler(event);" draggable="true"></textarea>
          </div>
       </div>
       <div>
@@ -78,4 +90,23 @@ fileInput.onchange = () => {
       const fileName = document.querySelector('#file-js-example .file-name');
       fileName.textContent = fileInput.files[0].name;
    }
+}
+
+
+function dragstart_handler(ev) {
+   // Set the drag's format and data. Use the event target's id for the data 
+   ev.dataTransfer.setData("text", ev.target.value);
+}
+
+function dragover_handler(ev) {
+   ev.preventDefault();
+}
+
+function drop_handler(ev) {
+   ev.preventDefault();
+   // Get the data, which is the id of the drop target
+   var data = ev.dataTransfer.getData("text");
+   ev.target.value = data;
+   // Clear the drag data cache (for all formats/types)
+   ev.dataTransfer.clearData();
 }
