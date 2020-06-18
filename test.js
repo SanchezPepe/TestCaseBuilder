3e58705aace02d56d675f86f018f2a23d41cd24a
sanchezpepe <sanchezpepe97@gmail.com>
2020-06-18 01:04:15 -0500

    automatic tests

--- test.js
+++ test.js
@@ -1,34 +1,123 @@
-var data = {
-   1 : {
-      "title": "TEST",
-      "preconditons": "KJFÑLSAKDFJDSAÑLFKJ",
-      "steps": "LKJSLÑKFJDSÑLFKJ",
-      "results": "KLFJSDAÑLFKJDSASAÑ"
+var data = [{
+      "id": 3,
+      "title": "TEST A",
+      "data": "A",
+      "preconditons": "A",
+      "steps": "A",
+      "results": "A"
+   },
+   {
+      "id": 3,
+      "title": "TEST B",
+      "data": "B",
+      "preconditons": "B",
+      "steps": "B",
+      "results": "B"
+   },
+   {
+      "id": 3,
+      "title": "TEST C",
+      "data": "A",
+      "preconditons": "A",
+      "steps": "A",
+      "results": "A"
+   },
+   {
+      "id": 3,
+      "title": "TEST D",
+      "data": "B",
+      "preconditons": "B",
+      "steps": "B",
+      "results": "B"
    }
+]
+
+function loadCasesTR() {
+   let timeout = 1000;
+   var testCase;
+   for (let i = 0; i < data2.length; i++) {
+      // Add new case button
+      setTimeout(function () {
+         testCase = data2[i];
+         console.log(testCase);
+         document.getElementsByClassName("link")[2].click();
+      }, ((i * 7) + 1) * timeout);
+
+      // Title input
+      setTimeout(function () {
+         document.getElementsByClassName("form-control-large")[0].value = testCase.title;
+      }, ((i * 7) + 2) * timeout);
+
+      // Submit new case
+      setTimeout(function () {
+         document.getElementsByClassName("icon-button-accept")[0].click();
+      }, ((i * 7) + 3) * timeout);
+
+      // Open the new case
+      setTimeout(function () {
+         var newCase = document.getElementsByClassName("caseRow");
+         newCase = newCase[newCase.length - 1];
+         //Open edit panel
+         newCase.getElementsByTagName("a")[5].click();
+      }, ((i * 7) + 4) * timeout);
+
+      // Hit panel edit button
+      setTimeout(function () {
+         document.getElementsByClassName("button-edit")[1].click();
+      }, ((i * 7) + 5) * timeout);
+
+      // Fill test case info
+      setTimeout(function () {
+         document.getElementById("custom_test_data").value = testCase.data;
+         document.getElementById("custom_preconds").value = testCase.preconditons;
+         document.getElementById("custom_steps").value = testCase.steps;
+         document.getElementById("custom_expected").value = testCase.results;
+      }, ((i * 7) + 6) * timeout);
+
+      // Save test case
+      setTimeout(function () {
+         document.getElementById("accept").removeAttribute("disabled");
+         document.getElementById("accept").click();
+      }, ((i * 7) + 7) * timeout);
+   };
 }
 
 
-function createCases(folder) {
-   var x = document.getElementById("section_id");
-   var y = x.getElementsByTagName("option");
-   let i = 0;
-   let folderValue = -1;
-   while (i < y.length) {
-      if (y[i].innerText.includes(folder)) {
-         folderValue = y[i].value;
-         break;
-      }
-      i++;
-   }
-   if (folderValue !== -1) {
-      x.value = folderValue;
-      let fieldsToFill = ["title","custom_test_data","custom_preconds","custom_steps","custom_expected"]
-      fieldsToFill.forEach(f => {
-         let elem = document.getElementById(f);
-         elem.value = "TEST TEST TEST";
-      })
-   } else {
-      alert("Folder not found");
-   }
-   return folderValue;
+function checkTime(n) {
+   let timeout = 1000;
+   for (let i = 0; i < n; i++) {
+      // Add new case button
+      setTimeout(function () {
+         console.log("Segundos: ", ((i * 7) + 1), "i = ", i + 1)
+      }, ((i * 7) + 1) * timeout);
+      // Title input
+      setTimeout(function () {
+         console.log("Segundos: ", ((i * 7) + 2), "i = ", i + 1)
+      }, ((i * 7) + 2) * timeout);
+
+      // Submit new case
+      setTimeout(function () {
+         console.log("Segundos: ", ((i * 7) + 3), "i = ", i + 1)
+      }, ((i * 7) + 3) * timeout);
+
+      // Open the new case
+      setTimeout(function () {
+         console.log("Segundos: ", ((i * 7) + 4), "i = ", i + 1)
+      }, ((i * 7) + 4) * timeout);
+
+      // Hit panel edit button
+      setTimeout(function () {
+         console.log("Segundos: ", ((i * 7) + 5), "i = ", i + 1)
+      }, ((i * 7) + 5) * timeout);
+
+      // Fill test case info
+      setTimeout(function () {
+         console.log("Segundos: ", ((i * 7) + 6), "i = ", i + 1)
+      }, ((i * 7) + 6) * timeout);
+
+      // Save test case
+      setTimeout(function () {
+         console.log("Segundos: ", ((i * 7) + 7), "i = ", i + 1)
+      }, ((i * 7) + 7) * timeout);
+   };
 }
